8997e70d37edda0472b3c45513e5439f82ea7daf
eduardomoraleslyn <v24cv5sfvv@privaterelay.appleid.com>
2026-05-27 20:09:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -418,6 +418,7 @@ def modal_eliminado_exitoso():
 # =========================================================
 
 df = cargar_datos()
+st.write(df.columns.tolist())
 
 # =========================================================
 # MODALES
