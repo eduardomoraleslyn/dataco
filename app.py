b217b1d5872d7be047c80b04022e6d29180b06b2
eduardomoraleslyn <v24cv5sfvv@privaterelay.appleid.com>
2026-05-28 01:55:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ div[data-testid="stDialog"] button[kind="primary"] {
     margin-top: 8px !important;
 }
 .contact-row {
-    padding: 3rem 1rem 2rem 1rem;
+    padding: 1rem 3rem 1rem 3rem;
     border-bottom: 1px solid #E2E8F0;
 }
 
